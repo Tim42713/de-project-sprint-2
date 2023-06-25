1ea4c2a842f61ba947f2d4d2eaa367ec954ff353
Timur <tbynin@hotmail.com>
2023-06-25 14:32:51 +0000

    upd

--- migrations/DML.sql
+++ migrations/DML.sql
@@ -28,13 +28,14 @@ SELECT DISTINCT
        shipping_transfer_rate
 FROM shipping
 
--- check SELECT * FROM public.shipping_transfer LIMIT 10;
+-- check 
+-- SELECT * FROM public.shipping_transfer LIMIT 10;
 
 -- заполняем таблицу public.shipping_info
 INSERT INTO public.shipping_info(shipping_id, vendor_id, payment_amount, shipping_plan_datetime, shipping_transfer_id, shipping_agreement_id, shipping_country_rate_id)
 SELECT DISTINCT
-       s.shipping_id AS shipping_id,
-       s.vendor_id AS vendor_id,
+       s.shippingid AS shipping_id,
+       s.vendorid AS vendor_id,
        s.payment_amount AS payment_amount,
        s.shipping_plan_datetime AS shipping_plan_datetime,
        st.id AS shipping_transfer_id,
@@ -44,7 +45,38 @@ FROM shipping AS s
 LEFT JOIN public.shipping_transfer AS st ON s.shipping_transfer_description = concat_ws(':', st.transfer_type, st.transfer_model)
 LEFT JOIN public.shipping_country_rates AS scr ON s.shipping_country = scr.shipping_country AND s.shipping_country_base_rate = scr.shipping_country_base_rate;
 
--- check SELECT * FROM public.shipping_info LIMIT 10;
+-- check
+-- SELECT * FROM public.shipping_info LIMIT 10;
 
 -- заполняем таблицу public.shipping_status
+WITH ms AS (
+       SELECT DISTINCT ON(shippingid),
+       shippingid,
+       status,
+       state,
+       state_datetime,
+       ROW NUMBER() OVER (PARTITION BY shippingid ORDER BY state_datetime DESC) AS r
+       FROM public.shipping
+       ORDER BY shippingid, state_datetime DESC
+),
+fd AS (
+       SELECT shippingid,
+              MAX(CASE WHEN state = 'booked' THEN state_datetime END) AS shipping_start_fact_datetime,
+              MAX(CASE WHEN state = 'recieved' THEN state_datetime END) AS shipping_end_fact_datetime
+              FROM public.shipping
+              GROUP BY shippingid
+)
 INSERT INTO public.shipping_status(shipping_id, status, state, shipping_start_fact_datetime, shipping_end_fact_datetime)
+SELECT m.shippingid AS shipping_id,
+       m.status AS status,
+       m.state AS state,
+       f.shipping_start_fact_datetime AS shipping_start_fact_datetime,
+       f.shipping_end_fact_datetime AS shipping_end_fact_datetime
+FROM ms AS m
+LEFT JOIN fd AS f ON m.shipping_id = f.shippingid
+WHERE m.r = 1;
+
+-- check
+-- SELECT * FROM public.shipping_status LIMIT 10;
+
+-- создаем представление shipping_datamart
